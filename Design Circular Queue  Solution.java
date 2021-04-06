a47e1c66210e3d663edeec961f2741910b0951c5
Kavin S <kavinalmighty@gmail.com>
2021-04-06 14:39:00 +0530

    leet code

---
+++ Design Circular Queue  Solution.java
@@ -0,0 +1,95 @@
+  Design Circular Queue
+
+Solution
+Design your implementation of the circular queue. The circular queue is a linear data structure in which the operations are performed based on FIFO (First In First Out) principle and the last position is connected back to the first position to make a circle. It is also called "Ring Buffer".
+
+One of the benefits of the circular queue is that we can make use of the spaces in front of the queue. In a normal queue, once the queue becomes full, we cannot insert the next element even if there is a space in front of the queue. But using the circular queue, we can use the space to store new values.
+
+Implementation the MyCircularQueue class:
+
+MyCircularQueue(k) Initializes the object with the size of the queue to be k.
+int Front() Gets the front item from the queue. If the queue is empty, return -1.
+int Rear() Gets the last item from the queue. If the queue is empty, return -1.
+boolean enQueue(int value) Inserts an element into the circular queue. Return true if the operation is successful.
+boolean deQueue() Deletes an element from the circular queue. Return true if the operation is successful.
+boolean isEmpty() Checks whether the circular queue is empty or not.
+boolean isFull() Checks whether the circular queue is full or not.
+ 
+
+Example 1:
+
+Input
+["MyCircularQueue", "enQueue", "enQueue", "enQueue", "enQueue", "Rear", "isFull", "deQueue", "enQueue", "Rear"]
+[[3], [1], [2], [3], [4], [], [], [], [4], []]
+Output
+[null, true, true, true, false, 3, true, true, true, 4]
+
+Explanation
+MyCircularQueue myCircularQueue = new MyCircularQueue(3);
+myCircularQueue.enQueue(1); // return True
+myCircularQueue.enQueue(2); // return True
+myCircularQueue.enQueue(3); // return True
+myCircularQueue.enQueue(4); // return False
+myCircularQueue.Rear();     // return 3
+myCircularQueue.isFull();   // return True
+myCircularQueue.deQueue();  // return True
+myCircularQueue.enQueue(4); // return True
+myCircularQueue.Rear();     // return 4
+Constraints:
+
+1 <= k <= 1000
+0 <= value <= 1000
+At most 3000 calls will be made to enQueue, deQueue, Front, Rear, isEmpty, and isFull.
+ 
+
+Solution:
+class MyCircularQueue {
+Queue<Integer> queue=new LinkedList<Integer>();
+  int size;
+  int rear=-1;
+    public MyCircularQueue(int k) {
+size=k;
+    }  
+    public boolean enQueue(int value) {
+     if(queue.size()<size)
+       rear=value;
+       return queue.size()<size?queue.add(value):false;
+    }
+    
+    public boolean deQueue() {      
+      if(queue.size()==1)
+      {
+        rear=-1;
+      }
+      
+      return queue.poll()!=null;
+    }
+    
+    public int Front() {
+      
+      return queue.size()>0? queue.peek():-1;
+    }
+    
+    public int Rear() {
+      return rear;
+    }
+    
+    public boolean isEmpty() {
+        return  queue.isEmpty();
+    }
+    
+    public boolean isFull() {
+        return queue.size()==size;
+    }
+}
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue obj = new MyCircularQueue(k);
+ * boolean param_1 = obj.enQueue(value);
+ * boolean param_2 = obj.deQueue();
+ * int param_3 = obj.Front();
+ * int param_4 = obj.Rear();
+ * boolean param_5 = obj.isEmpty();
+ * boolean param_6 = obj.isFull();
+ */
